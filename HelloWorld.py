22bf5213b34ad54c0016f25abb99ac43dbb620a3
arkadeepg <112115235+arkadeepg@users.noreply.github.com>
2023-06-28 22:13:12 +0530

    Initial Commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,23 @@
 print("Hello World")
+
+# Single line comment, shortcut: ctrl + /
+
+"""
+Multiline 
+Comment
+"""
+
+'''
+Multiline 
+Comment 2
+'''
+
+# Another
+# Multiline
+# Comment
+
+# Seperator, between each element
+print("Ram", "Shyam", "Kumar", sep=">")
+
+# Endline, at the very end of statement
+print("Ram", "Shyam", "Kumar", end=" 00 ")
